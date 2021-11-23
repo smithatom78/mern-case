061a0c04e4d719ef51394ba0233ce647a2656aa0
Smitha Jacob <smithatom78@gmail.com>
2021-11-23 11:50:27 +0530

    ch

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+var username = document.getElementById("Username");
+var password1 = document.getElementById("Password1");
+
+function test(callback) {
+    if (username.value == "admin" && password1.value == "12345") {
+        alert("Authorized Login Successful");
+        //    alert("hi");
+        callback();
+    } else if (username.value == "admin" && password1.value != "12345") {
+        alert("Incorrect Password");
+    } else if (username.value != "admin" && password1.value == "12345") {
+        alert("User Not Registered");
+    } else {
+        alert("Incorrect UserID and Password");
+    }
+}
+
+function myFunction() {
+    //  window.location.href = "https://smithatom78.github.io/samp1/mern-case/welcome.html"
+    window.location.href = "\welcome.html";
+    //window.location.href = "https://smithatom78.github.io/samp1/mern-case/welcome.html";
+
+}
